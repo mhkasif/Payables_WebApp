1eeb1038902d723fc6fd27658583f30c8d18a0b5
Safeer Malik <67726497+safeermalik1515@users.noreply.github.com>
2020-07-09 11:42:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { resolve } = require('path');
 const bodyParser = require('body-parser');
 // Replace if using a different env file or config
 
-require('dotenv').config({ path: './exp.env' });
+require('dotenv').config({ path: 'exp.env' });
 
 if (
   !process.env.STRIPE_SECRET_KEY ||
@@ -48,7 +48,7 @@ if (
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.use(express.static('./'+process.env.STATIC_DIR));
+app.use(express.static(process.env.STATIC_DIR));
 
 // Use JSON parser for all non-webhook routes.
 app.use((req, res, next) => {
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  const path = './frontend/index.html';
+  const path = 'frontend/index.html';
     res.sendFile(path);
 });
 
