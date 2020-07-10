a27f4f87be821f087c2fbbcaf2e0c5c77696523a
Safeer Malik <67726497+safeermalik1515@users.noreply.github.com>
2020-07-10 10:56:14 +0500

    firebase

--- server.js
+++ server.js
@@ -72,7 +72,15 @@ app.get('/config', async (req, res) => {
 });
 
 app.get('/firebaseConfig', async (req, res) => {
- 
+ console.log({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASEURL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.FIREBASE_APP_ID,
+});
   res.send({
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
